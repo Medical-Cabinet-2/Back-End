441958c1a255b414e25fe40acf26ffebb970f566
Brad <brad@viziworx.com>
2020-01-06 13:09:16 -0500

    added routes to server

--- api/server.js
+++ api/server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
 
+const authRouter = require('./auth/authRouter');
+const userRouter = require('./user/userRouter');
+
 const server = express();
 
 server.use(helmet());
@@ -9,5 +12,8 @@ server.use(cors());
 server.use(express.json());
 
 server.use('/api/auth', authRouter);
+server.use('/api/user', userRouter);
+
+server.get('/', (req, res) => res.status(200).json({ message: 'API is online.' }));
 
 module.exports = server;
