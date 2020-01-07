2ad184a14979a5112bcbb06aa6413d694fa9cf08
Brad <brad@viziworx.com>
2020-01-07 13:10:59 -0500

    added .returning to add function

--- data/helpers/users/usersModel.js
+++ data/helpers/users/usersModel.js
@@ -16,7 +16,7 @@ function findById(id) {
 }
 
 async function add(user) {
-    const [id] = await db('users').insert(user, 'id');
+    const [id] = await db('users').insert(user, 'id').returning('*');
 
     return findById(id);
 }
